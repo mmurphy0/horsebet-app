4433860b89a5c6074ee86e0ceb294e4ceeb8f298
Max Murphy <mmurph078@gmail.com>
2025-11-15 22:17:09 +0000

    final program

--- horsebet.py
+++ horsebet.py
@@ -1,29 +1,180 @@
 import random
-import time
+import sys
 import tkinter as tk
 from tkinter import Toplevel
 from tkinter import messagebox
-from tkinter import Label
-
-#verifies age is 18 or over for access
-#asks for login details
-#shows homepage with horses and odds
-#no functionality to place bets yet - coming soon
-#users deposit money for each round
-#forced to withdraw after each round
-
-balance = float(0.0)
 
 logins = {
     'admin' : 'admin',
     'user1' : 'password1',
 }
 
+horses = {
+    'Thunder' : 2,
+    'Storm' : 5,
+    'Shadow' : 3,
+    'Ghost' : 4
+}
+
+def horserace():
+    global amount
+    
+    win_horse = random.choice(list(horses.keys()))
+
+    if horse == 'Thunder':
+        win = (amount*2)
+        lose = (amount-amount)
+
+    elif horse == 'Storm':
+        win = (amount*5)
+        lose = (amount-amount)
+
+    elif horse == 'Shadow':
+        win = (amount*3)
+        lose = (amount-amount)
+
+    elif horse == 'Ghost':
+        win = (amount*4)
+        lose = (amount-amount)
+
+    if horse == win_horse:
+        amount = win
+        messagebox.showinfo('Result', f'Congratulations {win_horse} won the race')
+        messagebox.showinfo('Winning',f'Your winnings of {amount} will be withdrawn')
+        home.destroy
+        homepage()
+    else:
+        amount = lose
+        messagebox.showinfo('Result',f'Unfortunately {win_horse} won the race')
+        home.destroy
+        homepage()
+        
+def checkhorse():
+    global horse
+
+    horse = horsewin_entry.get()
+
+    if horse in horses:
+        horsewin.destroy()
+        messagebox.showinfo('Confirmation','The race will start shortly')
+        horserace()
+    else:
+        horsewin.destroy()
+        messagebox.showinfo('Error','Horse does not exist')
+        choosehorse()
+
+def choosehorse():
+    global horsewin, horsewin_entry
+    horsewin = Toplevel()
+    horsewin.title('Choose Your Horse')
+    horsewin.geometry('300x100')
+    horsewin.minsize(300,100)
+    horsewin.maxsize(300,100)
+
+    depositwin.destroy()
+
+    horsewin_title = tk.Label(
+        horsewin,
+        text='Please type in your chosen horse',
+        font=('Arial',15,'bold')
+    )
+    horsewin_title.pack()
+
+    horsewin_entry = tk.Entry(horsewin)
+    horsewin_entry.pack()
+
+    horsewin_button = tk.Button(
+        horsewin,
+        text='Submit',
+        font=('Arial',15),
+        command=checkhorse
+    )
+    horsewin_button.pack()
+
+def checkdeposit():
+    global amount
+    try:
+        amount = float(deposit_entry.get())
+    except ValueError:
+        messagebox.showinfo('Error','A value must be entered')
+        return
+
+    if amount == 0:
+        messagebox.showinfo('Error','A value must be entered')
+        depositwin.destroy()
+        depositfunc()
+    else:
+        messagebox.showinfo('Confirmation','Successfully added to your balance')
+        choosehorse()
+
+def depositfunc():
+    global deposit_entry, depositwin
+
+    depositwin = Toplevel()
+    depositwin.title('Enter Deposit')
+    depositwin.geometry('220x140')
+    depositwin.minsize(220,130)
+    depositwin.maxsize(220,130)
+
+    deposit_title = tk.Label(
+        depositwin,
+        text='Deposit',
+        font=('Arial',15,'bold')
+    )
+    deposit_title.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    deposit_label = tk.Label(
+        depositwin,
+        text='£',
+        font=('Arial')
+    )
+    deposit_label.grid(
+        row=2,
+        column=1
+    )
+
+    deposit_entry = tk.Entry(depositwin)
+    deposit_entry.grid(
+        row=2,
+        column=2
+    )
+
+    deposit_button = tk.Button(
+        depositwin,
+        text='Add To Balance',
+        font=('Arial',15),
+        width=20,
+        command = checkdeposit
+    )
+    deposit_button.grid(
+        row=3,
+        column=1,
+        columnspan=2
+    )
+
+    deposit_cancel = tk.Button(
+        depositwin,
+        text='Back',
+        font=('Arial',15),
+        width=20,
+        command=depositwin.destroy
+    )
+    deposit_cancel.grid(
+        row=4,
+        column=1,
+        columnspan=2
+    )
+
 def checkusr():
     usr = l3.get()
     pwd = l5.get()
 
     if usr in logins and logins[usr] == pwd:
+        loginpage.destroy()
         homepage()
     
     else:
@@ -32,27 +183,97 @@ def checkusr():
 
 def checkage():
     age = int(age_entry.get())
-
     if age >= 18:
         login()
     
     else:
         messagebox.showinfo('Error','You must be 18 or older to play')
-        root.destroy()
 
 def logout():
     messagebox.showinfo('Confirmation','Successfully logged out')
     login()
 
+def registered():
+    usr = usr_entry.get()
+    pwd = pwd_entry.get()
+
+    logins.update({usr : pwd})
+    messagebox.showinfo('Confirmation','Account Successfully Created')
+    homepage()
+
+def register():
+    global register_acc, usr_entry, pwd_entry
+    register_acc = Toplevel()
+    register_acc.title('Register an account')
+    register_acc.geometry('280x118')
+    register_acc.minsize(280,118)
+    register_acc.maxsize(280,118)
+    
+    usr_label = tk.Label(
+        register_acc,
+        text='Username',
+        font=('Arial',15)
+    )
+    usr_label.grid(
+        row=1,
+        column=1
+    )
+
+    usr_entry = tk.Entry(register_acc)
+    usr_entry.grid(
+        row=1,
+        column=2
+    )
+
+    pwd_label = tk.Label(
+        register_acc,
+        text='Password',
+        font=('Arial',15)
+    )
+    pwd_label.grid(
+        row=2,
+        column=1    
+    )
+
+    pwd_entry = tk.Entry(register_acc)
+    pwd_entry.grid(
+        row=2,
+        column=2
+    )
+
+    register_button = tk.Button(
+        register_acc,
+        text='Register',
+        width=20,
+        font=('Arial',15),
+        command=registered
+    )
+    register_button.grid(
+        row=3,
+        column=1,
+        columnspan=2
+    )
+
+    exit_button = tk.Button(
+        register_acc,
+        text='Exit',
+        width=20,
+        font=('Arial',15),
+        command=sys.exit
+    )
+    exit_button.grid(
+        row=4,
+        column=1,
+        columnspan=2
+    )
 
 def login():
+    global l3,l5,loginpage
     loginpage = Toplevel()
     loginpage.title('Login')
-    loginpage.geometry('280x130')
-    loginpage.minsize(280,145)
-
-    global l3
-    global l5
+    loginpage.geometry('280x180')
+    loginpage.minsize(280,180)
+    loginpage.maxsize(280,180)
 
     l1 = tk.Label(
         loginpage,
@@ -113,9 +334,9 @@ def login():
 
     l7 = tk.Button(
         loginpage,
-        text='Exit',
+        text='Register',
         width=20,
-        command=loginpage.destroy
+        command=register
     )
     l7.grid(
         row=5,
@@ -124,14 +345,31 @@ def login():
         pady=0
     )
 
+    l8 = tk.Button(
+        loginpage,
+        text='Exit',
+        width=20,
+        command=sys.exit
+    )
+    l8.grid(
+        row=6,
+        column=1,
+        columnspan=2,
+        pady=0
+    )
+
 def homepage():
-    messagebox.showinfo('Error','Your balance will be reset to £10 each round')    
+    messagebox.showinfo('Error','Your balance will be withdrawn after each race')    
     
+    global amount, home
+    amount = float(0)
+
     home = Toplevel()
     home.update_idletasks()
-    home.geometry('335x200')
+    home.geometry('340x200')
     home.title('Home')
-    home.minsize(340, 200)
+    home.minsize(340,200)
+    home.maxsize(340,200)
     
     home_title = tk.Label(
         home,
@@ -200,7 +438,8 @@ def homepage():
     bet_button = tk.Button(
         home,
         text='Place Bet',
-        width=20
+        width=20,
+        command=depositfunc
     )
     bet_button.grid(
         row=4,
@@ -209,19 +448,6 @@ def homepage():
         pady=0
     )
     
-    deposit_button = tk.Button(
-        home,
-        text='Deposit to balance',
-        width=20,
-        command=deposit
-    )
-    deposit_button.grid(
-        row=5,
-        column=3,
-        columnspan=2,
-        pady=0
-    )
-
     logout_button = tk.Button(
         home,
         text='Logout',
@@ -229,16 +455,17 @@ def homepage():
         command=logout
     )
     logout_button.grid(
-        row=6,
+        row=5,
         column=3,
         columnspan=2,
         pady=0
     )
 
-
 root = tk.Tk()
 root.title('Verify Age')
 root.geometry('230x100')
+root.minsize(230,100)
+root.maxsize(230,100)
 
 age_title = tk.Label(
     root,
